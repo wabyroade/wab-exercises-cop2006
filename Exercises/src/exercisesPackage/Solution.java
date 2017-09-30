1509651f0aa758c3d35fb8991ec25a0ab74c5159
Wyatt <Wyatt@10.0.0.245>
2017-09-29 22:37:57 -0400

    First commit of Exercises Project from laptop.

---
+++ Exercises/src/exercisesPackage/Solution.java
@@ -0,0 +1,10 @@
+package exercisesPackage;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing Exercise");
+	}
+
+}
