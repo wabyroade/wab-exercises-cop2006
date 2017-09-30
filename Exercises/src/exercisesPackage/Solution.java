c7f248aa00d72f92c3797dd108aa31d5ecb43440
Wyatt <Wyatt@10.0.0.245>
2017-09-29 22:40:20 -0400

    Testing first commit and push from laptop.

--- Exercises/src/exercisesPackage/Solution.java
+++ Exercises/src/exercisesPackage/Solution.java
@@ -4,7 +4,7 @@ public class Solution {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testing Exercise");
+		System.out.println("Testing Exercise.");
 	}
 
 }
